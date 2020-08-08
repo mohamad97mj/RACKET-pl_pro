a0ef6529b5789031690b2e03baf4df1078865f46
mohamad <mohamad97mj@gmail.com>
2020-08-08 17:37:44 +0430

    initial commit

--- solution/NUMEX-master/src/test.rkt
+++
@@ -1,5 +0,0 @@
-;; CASE 1: Recursion | Function Call | Parameter Passing
-(define program4 (call (fun "func1" "arg1" (ifzero (var "arg1") (int 4) (mlet "insider" (add (var "arg1") (int -1)) (add (call (var "func1") (var "insider")) (int 1))))) (int 5)))
-
-;; CASE 2: mlet handling
-(define program5 (mlet "a" (int 1) (mlet "b" (int 2) (add (var "a") (var "b")))))
