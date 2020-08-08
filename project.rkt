128a76178fcd0073a2b6143ab86a258e0de2ff6e
mohamad <mohamad97mj@gmail.com>
2020-08-08 18:56:23 +0430

    struct defined

--- project.rkt
+++ project.rkt
@@ -18,19 +18,20 @@
 (struct cnd  (e1 e2 e3)  #:transparent)  ;; conditional
 (struct iseq (e1 e2)  #:transparent)  ;; comparison
 (struct ifnzero (e1 e2)  #:transparent)  ;; if not zero
-
-
-
+(struct ifleq (e1 e2 e3 e4)  #:transparent)  ;; if less than or equal
+;(struct lam (s1 s2 e)  #:transparent)  ;; lambda
 (struct lam  (nameopt formal body) #:transparent) ;; a recursive(?) 1-argument function
 (struct apply (funexp actual)       #:transparent) ;; function application
-
+;(struct myapply (funexp actual)  #:transparent) ;; function application
+(struct apair (e1 e2)  #:transparent) ;; pair
 
 (struct munit   ()      #:transparent) ;; unit value -- good for ending a list
 (struct ismunit (e)     #:transparent) ;; if e1 is unit then true else false
-
+(struct 1st (e1)   #:transparent) ;; the first part of the pair
+(struct 2nd (e1)   #:transparent) ;; the second part of the pair
+(struct ismunit (e)     #:transparent) ;; testing for munit
 ;; a closure is not in "source" programs; it is what functions evaluate to
-(struct closure (env f) #:transparent) 
-
+(struct closure (env f) #:transparent)
 (struct letrec (s1 e1 s2 e2 s3 e3 e4) #:transparent) ;; a letrec expression for recursive definitions
 (struct queue (e q) #:transparent) ;; it holds several expressions
 (struct enqueue (e q) #:transparent) ;; it enqueues e into q
@@ -38,9 +39,19 @@
 (struct extract (q) #:transparent) ;; it returns queue's top element
 
 ;; Problem 1
+(define (racketlist->numexlist xs)
+  (cond [(null? xs) (munit)]
+        [(list? xs) (apair (car xs) (racketlist->numexlist (cdr xs)))]
+        [#t (error ("invalid racket list"))]
+  )
+)
 
-(define (racketlist->numexlist xs) "CHANGE")
-(define (numexlist->racketlist xs) "CHANGE")
+(define (numexlist->racketlist xs)
+  (cond [(munit? xs) null]
+        [(apair? xs) (cons (apair-e1 xs) (numexlist->racketlist (apair-e2 xs)))]
+        [#t (error ("invalid numex list"))]
+  )
+)
 
 ;; Problem 2
 
