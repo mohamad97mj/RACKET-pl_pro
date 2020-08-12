7dbb4f6575c2bbeb7bc136af876c08bd0dfca887
mohamad <mohamad97mj@gmail.com>
2020-08-12 17:01:41 +0430

    cnd implemented test-37

--- project.rkt
+++ project.rkt
@@ -17,19 +17,19 @@
 (struct orelse  (e1 e2)  #:transparent)  ;; logical disjunction
 (struct cnd  (e1 e2 e3)  #:transparent)  ;; conditional
 (struct iseq (e1 e2)  #:transparent)  ;; comparison
-(struct ifnzero (e1 e2)  #:transparent)  ;; if not zero
+(struct ifnzero (e1 e2 e3)  #:transparent)  ;; if not zero
 (struct ifleq (e1 e2 e3 e4)  #:transparent)  ;; if less than or equal
+(struct with (s e1 e2)  #:transparent) ;; a let expression where the value of e 1 is bound to s in e 2
 ;(struct lam (s1 s2 e)  #:transparent)  ;; lambda
 (struct lam  (nameopt formal body) #:transparent) ;; a recursive(?) 1-argument function
 (struct apply (funexp actual)       #:transparent) ;; function application
 ;(struct myapply (funexp actual)  #:transparent) ;; function application
 (struct apair (e1 e2)  #:transparent) ;; pair
 
-(struct munit   ()      #:transparent) ;; unit value -- good for ending a list
-(struct ismunit (e)     #:transparent) ;; if e1 is unit then true else false
 (struct 1st (e1)   #:transparent) ;; the first part of the pair
 (struct 2nd (e1)   #:transparent) ;; the second part of the pair
-(struct ismunit (e)     #:transparent) ;; testing for munit
+(struct munit   ()      #:transparent) ;; unit value -- good for ending a list
+(struct ismunit (e)     #:transparent) ;; if e1 is unit then true else false
 ;; a closure is not in "source" programs; it is what functions evaluate to
 (struct closure (env f) #:transparent)
 (struct letrec (s1 e1 s2 e2 s3 e3 e4) #:transparent) ;; a letrec expression for recursive definitions
@@ -58,10 +58,11 @@
 ;; lookup a variable in an environment
 ;; Complete this function
 (define (envlookup env str)
-  (cond [(null? env) (error "unbound variable during evaluation" str)]
-  		"CHANGE" 
-		)
- )
+  (cond [(null? env) (error "unbound variable during evaluation" str)])
+  (cond [(equal? str (car (car env))) (cdr (car env))]
+        [else (envlookup (cdr env) str)]
+  )
+)
 
 ;; Complete more cases for other kinds of NUMEX expressions.
 ;; We will test eval-under-env by calling it directly even though
@@ -69,15 +70,114 @@
 (define (eval-under-env e env)
   (cond [(var? e) 
          (envlookup env (var-string e))]
-        [(plus? e) 
-         (let ([v1 (eval-under-env (plus-e1 e) env)]
-               [v2 (eval-under-env (plus-e2 e) env)])
-           (if (and (num? v1)
-                    (num? v2))
-               (num (+ (num-int v1) 
-                       (num-int v2)))
-               (error "NUMEX addition applied to non-number")))]
-        ;; CHANGE add more cases here
+        [(munit? e) e]
+
+
+        [(num? e)
+            (cond [(integer? (num-int e)) e]
+                       [else (error "NUMEX num must be a racket integer")])]
+
+        [(bool? e)
+                    (cond [(boolean? (bool-boolean e)) e]
+                               [else (error "NUMEX bool must be a racket boolean")])]
+
+        [(plus? e)
+            (let ([v1 (eval-under-env (plus-e1 e) env)]
+                  [v2 (eval-under-env (plus-e2 e) env)])
+                (if (and (num? v1) (num? v2))
+                   (num (+ (num-int v1) (num-int v2)))
+                   (error "NUMEX plus applied to non-num")))]
+
+        [(minus? e)
+            (let ([v1 (eval-under-env (minus-e1 e) env)]
+                  [v2 (eval-under-env (minus-e2 e) env)])
+                (if (and (num? v1) (num? v2))
+                   (num (- (num-int v1) (num-int v2)))
+                   (error "NUMEX minus applied to non-num")))]
+
+        [(mult? e)
+            (let ([v1 (eval-under-env (mult-e1 e) env)]
+                  [v2 (eval-under-env (mult-e2 e) env)])
+                (if (and (num? v1) (num? v2))
+                   (num (* (num-int v1) (num-int v2)))
+                   (error "NUMEX mult applied to non-num")))]
+
+        [(div? e)
+            (let ([v1 (eval-under-env (div-e1 e) env)]
+                  [v2 (eval-under-env (div-e2 e) env)])
+                (if (and (num? v1) (num? v2))
+;                   (if (equal? (num-int v2) 0) (bool #f) (num (quotient (num-int v1) (num-int v2))))
+                   (num (quotient (num-int v1) (num-int v2)))
+                   (error "NUMEX div applied to non-num")))]
+
+        [(neg? e)
+            (let ([v1 (eval-under-env (neg-e1 e) env)])
+                (cond
+                    [(num? v1) (num (- (num-int v1)))]
+                    [(bool? v1) (bool (not (bool-boolean v1)))]
+                    [#t (error "NUMEX negation applied to non-num")]))]
+
+        [(andalso? e)
+            (let ([v1 (eval-under-env (andalso-e1 e) env)])
+                  (if (equal? #f (bool-boolean v1)) v1
+                       (let ([v2 (eval-under-env (andalso-e2 e) env)])
+                            (if (and (or (bool? v1) (num? v1))(or (bool? v2) (num? v2)))
+                                (cond
+                                    [(and (bool? v1) (bool? v2)) (
+                                      bool (and (bool-boolean v1) (bool-boolean v2))
+                                    )]
+                                    [(and (bool? v1) (num? v2)) (if (bool-boolean v1) v2 v1)]
+                                    [(num? v1) v2]
+                                 )
+                                (error "NUMEX andalso applied to non-bool")))))]
+
+        [(orelse? e)
+            (let ([v1 (eval-under-env (orelse-e1 e) env)]
+                  [v2 (eval-under-env (orelse-e2 e) env)])
+                    (if (and (or (bool? v1) (num? v1))(or (bool? v2) (num? v2)))
+                        (cond
+                            [(and (bool? v1) (bool? v2)) (
+                              bool (or (bool-boolean v1) (bool-boolean v2))
+                            )]
+                            [(and (bool? v1) (num? v2)) (if (bool-boolean v1) v1 v2)]
+                            [(num? v1) v1]
+                         )
+                        (error "NUMEX orelse applied to non-bool")))]
+
+        [(iseq? e)
+            (let ([v1 (eval-under-env (iseq-e1 e) env)]
+                  [v2 (eval-under-env (iseq-e2 e) env)])
+                    (if (and (or (bool? v1) (num? v1))(or (bool? v2) (num? v2)))
+                        (cond
+                            [(and (bool? v1) (bool? v2))
+                            (if (equal? (bool-boolean v1) (bool-boolean v2)) (bool #t) (bool #f))]
+                            [(and (num? v1) (num? v2))
+                            (if (equal? (num-int v1) (num-int v2)) (bool #t) (bool #f))]
+                            [#t (bool #f)]
+                        )
+                        (error "NUMEX iseq applied to non-bool non-num")))]
+
+        [(ismunit? e)
+            (let ([v (eval-under-env (ismunit-e e) env)])
+;               (if (munit? v) (num 1) (num 0)))] this line is corrcect based on project definition but in test if fails!
+               (if (munit? v) (bool #t) (bool #f)))]
+
+
+        [(apair? e)
+            (let ([v1 (eval-under-env (apair-e1 e) env)]
+                  [v2 (eval-under-env (apair-e2 e) env)])
+                    (apair v1 v2))]
+
+        [(cnd? e)
+            (let ([v (eval-under-env (cnd-e1 e) env)])
+                (if (bool? v)
+                  (if (bool-boolean v)
+                    (eval-under-env (cnd-e2 e) env)
+                    (eval-under-env (cnd-e3 e) env))
+                (error "NUMEX cnd first argument must be boolean")))]
+
+
+
         [#t (error (format "bad NUMEX expression: ~v" e))]))
 
 ;; Do NOT change
@@ -86,16 +186,25 @@
         
 ;; Problem 3
 
-(define (ifmunit e1 e2 e3) "CHANGE")
 
-(define (with* bs e2) "CHANGE")
+
+;(define (with* bs e2) "CHANGE")
+(define (with* bs e2)
+  (if (equal? bs null)
+      e2
+      (with (car (car bs)) (cdr (car bs)) (with* (cdr bs) e2))))
+
 
 (define (ifneq e1 e2 e3 e4) "CHANGE")
 
-;; Problem 4
+(define (ifmunit e1 e2 e3) "CHANGE")
 
+
+;
+;;; Problem 4
+;
 (define numex-filter "CHANGE")
-
+;
 (define numex-all-gt
   (with "filter" numex-filter
         "CHANGE (notice filter is now in NUMEX scope)"))
